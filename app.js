547d1f7202ea797a37b7cd6dd42deae0dedc36a7
sergioQA <sshamotenko@gmail.com>
2022-04-01 13:32:34 +0700

    First Init

---
+++ app.js
@@ -0,0 +1,66 @@
+
+
+function createCalendar(elem, year, month) {
+    elem = document.querySelector(elem)
+
+    let mon = month - 1;
+    let d = new Date(year, mon)
+
+    let table = `
+        
+        <table>
+          <tr>
+            <th>пн</th>
+            <th>вт</th>
+            <th>ср</th>
+            <th>чт</th>
+            <th>пт</th>
+            <th>сб</th>
+            <th>вс</th>
+          </tr>
+          <tr>
+    `;
+
+    for (let i = 0; i < getDay(d); i++) {
+      table += '<td></td>'
+      
+    }
+
+    while (d.getMonth() == mon) {
+      table += '<td>' + d.getDate() + '</td>';
+      if (getDay(d) % 7 == 6) {
+        table += '</tr><tr>'
+      }
+      d.setDate(d.getDate()+1)
+    }
+
+    if (getDay(d) != 0) {
+      for (let i = getDay(d); i < 7; i++) {
+      table += '<td></td>'
+      
+      }
+      
+    }
+
+
+    table += '</tr></table>';
+    elem.innerHTML = table;
+  }
+
+  function getDay(date){
+    let day = date.getDay();
+    if (day == 0) day = 7;
+    return day - 1;
+  }
+  function myFunction() {
+    
+    let z = document.getElementById("myDate").value;
+        
+    createCalendar('.calendar', z.slice(0,4), z.slice(5,7))  
+    
+    
+  }
+  
+
+  //createCalendar('.calendar', 2021, 4)
+  
